ac33dbc9113f11a8a3fc8e692837290226238f4e
Andrew Ngo <aandrewnngo@gmail.com>
2017-11-19 17:51:50 -0600

    FXML functionality

---
+++ Encryption Game/src/controller/MainController.java
@@ -0,0 +1,10 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController implements EventHandler<ActionEvent> {
+
+}
--- Encryption Game/src/core/EncryptDriver.java
+++ Encryption Game/src/core/EncryptDriver.java
@@ -1,16 +1,44 @@
 package core;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 /**
  * Simple driver class that runs the test version of the Encryption Game.
  * @author Connor Guerra
  *
  */
-public class EncryptDriver{
+public class EncryptDriver extends Application{
 	
-	public static void main(String[] args){
+	private static Stage stage;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+			primaryStage.setScene(new Scene(root, 480, 640));
+			primaryStage.setTitle("Project Encryption");
+			primaryStage.getIcons().add(new Image("/view/icon-512@x.png"));
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		this.stage = primaryStage;
 		
+	}
+	public static void main(String[] args){
+		launch(args);
 		EncryptGame game = new EncryptGame("Testificate");
 		game.runGame();
+	}
+	public static Stage getStage()
+	{
+		return stage;
 	}
 
 }
